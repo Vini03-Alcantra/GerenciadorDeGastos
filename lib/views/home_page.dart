300f6a9fa5f5120a349746b070c1e8902d2f0719
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-09-16 14:56:49 -0300

    Criando customAppBar

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,8 +10,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return Scaffold(
+      body: Container(  
+        margin: EdgeInsets.only(top: 8),
+        child: ListView(  
+          physics:  ClampingScrollPhysics(),
+          children: <Widget>[
+            Container(  
+              margin: EdgeInsets.only(left: 16, right: 16, top: 16),
+              child: Row(  
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  SvgPicture.asset('assets/svg/drawer_icon.svg'),
+                  Container(  
+                    height: 59,
+                    width: 59,
+                    decoration: BoxDecoration(  
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(image: AssetImage('assets/images/user_image.png'))
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+      )
     );
   }
 }
