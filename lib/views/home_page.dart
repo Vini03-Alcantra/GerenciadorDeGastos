ba2f75f3a92b27b2c47283c946dceb5ef4e0da41
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-09-16 17:08:57 -0300

    Criando Handle Indicator

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -11,6 +11,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  //Handle Indicator 
+  List<T> map<T>(List list, Function handler){
+    List<T> result = [];
+    for(var i = 0; i < list.length; i++){
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
