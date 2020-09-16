5825a9f68d3973dee802242e52595e9ade065fd0
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-09-16 15:33:20 -0300

    estruturando cartões

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:moneymanagementcomplex/constants/color_constants.dart';
+import 'package:moneymanagementcomplex/model/card_model.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -44,11 +47,52 @@ class _HomePageState extends State<HomePage> {
             SizedBox(  
               height: 25,
             ),
+            //Widget nome e cumprimento
+            Padding(
+              padding: EdgeInsets.only(left: 16, bottom: 20),
+              child: Column(  
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Bom dia",
+                    style: GoogleFonts.inter(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: kBlackColor
+                    ),
+                  ),
+                  Text(
+                    "Vinícius de Alcantra",
+                    style: GoogleFonts.inter(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: kBlackColor
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //Termina aqui
 
-            Column(  
-              children: <Widget>[
-                
-              ],
+            Container(
+              height: 199,
+              child: ListView.builder(
+                physics: ClampingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.only(left: 16, right: 6),
+                itemCount: cards.length,
+                itemBuilder: (context, index){
+                  return Container(
+                    margin: EdgeInsets.only(right: 10),
+                    height: 199,
+                    width: 344,
+                    decoration: BoxDecoration(  
+                      borderRadius: BorderRadius.circular(28),
+                      color: Color(cards[index].cardBackground)
+                    )
+                  );
+                }
+              )
             )
           ],
         )
