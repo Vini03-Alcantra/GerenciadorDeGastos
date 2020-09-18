ce5d60db58e63d800955d2a23a4aeabfee798bf4
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-09-18 18:00:58 -0300

    criando widget oerations

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -5,6 +5,6 @@ const kTwentyBlueColor = Color(0x201E1E99);
 const kPinkColor = Color(0xFFFF70A3);
 const kWhiteColor = Color(0xFFFFFFFF);
 const kBlackColor = Color(0xFF3A3A3A);
-const kTenColor = Color(0x10000000);
+const kTenBlackColor = Color(0x10000000);
 const kBackgroundColor = Color(0xFFFAFAFA);
 const kGreyColor = Color(0xFF8a959e);
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -187,8 +187,9 @@ class _HomePageState extends State<HomePage> {
             ),
             //Cartão termina aqui
 
+            //Início Operations
             Padding(
-              padding: EdgeInsets.only(left: 16, bottom: 13, top: 29, right: 26),
+              padding: EdgeInsets.only(left: 16, bottom: 13, top: 29, right: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -205,6 +206,7 @@ class _HomePageState extends State<HomePage> {
                           alignment: Alignment.centerLeft,
                           height: 9,
                           width: 9,
+                          margin: EdgeInsets.only(right: 6),
                           decoration: BoxDecoration(  
                             shape: BoxShape.circle,
                             color: current == index 
@@ -217,10 +219,90 @@ class _HomePageState extends State<HomePage> {
                   )
                 ],
               )
+            ),
+            //Final Operations
+
+            Container(  
+              height: 123,
+              child: ListView.builder(
+                itemCount: datas.length,
+                padding: EdgeInsets.only(left: 16),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index){
+                  return GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        current = index;
+                      });
+                    },
+                    child: OperationCard(
+                      operation: datas[index].name,
+                      selectedIcon: datas[index].selectedIcon,
+                      unselectedIcon: datas[index].unselectedIcon,
+                      isSelected: current == index,
+                      context: this,
+                    ),
+                  );
+                }
+              )
             )
           ],
         )
       )
+    );
+  }
+}
+
+class OperationCard extends StatefulWidget {
+  final String operation;
+  final String selectedIcon;
+  final String unselectedIcon;
+  final bool isSelected;
+  _HomePageState context;
+
+
+  OperationCard({this.operation, this.selectedIcon, this.unselectedIcon, this.isSelected, this.context});
+  @override
+  _OperationCardState createState() => _OperationCardState();
+}
+
+class _OperationCardState extends State<OperationCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 16),
+      width: 123,
+      height: 123,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(  
+            color: kTenBlackColor,
+            blurRadius: 10,
+            spreadRadius: 5,
+            offset: Offset(8.0, 8.0)
+          )
+        ],
+        borderRadius: BorderRadius.circular(15),
+        color: widget.isSelected ? kBlueColor : kWhiteColor
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SvgPicture.asset(
+            widget.isSelected ? widget.selectedIcon : widget.unselectedIcon
+          ),
+          SizedBox(height: 9),
+          Text(
+            widget.operation, textAlign: TextAlign.center,
+            style: GoogleFonts.inter(
+              fontSize: 15,
+              fontWeight: FontWeight.w700,
+              color: widget.isSelected ? kWhiteColor : kBlueColor
+            ),
+          )
+        ],
+      ),
     );
   }
 }
